3a9466ffc80dfbb9725a49dccb0b74d42258f8f8
Andy Ramaroson <andyramaroson@gmail.com>
2022-11-23 20:07:15 +0100

    [ehancement] add feature component with refacto, scss and display in Home

--- argentbank/src/pages/Home/Home.jsx
+++ argentbank/src/pages/Home/Home.jsx
@@ -1,11 +1,26 @@
 import React from "react";
 import Hero from "../../components/Hero/Hero";
 import image from "./../../assets/bank-tree.jpeg"
+import Feature from "../../components/Feature/Feature";
+
+// import { featureData } from '../services/data';
 
 function Home() {
   return (
     <section>
       <Hero image={image}/>
+      <Feature />
+      {/* <main className="main home">
+        {featureData.map((item) => (
+          <Feature
+            key={item.id}
+            src={item.icon}
+            alt={item.alt}
+            title={item.title}
+            paragraph={item.paragraph}
+          />
+        ))}
+      </main> */}
     </section>
   )
 }
