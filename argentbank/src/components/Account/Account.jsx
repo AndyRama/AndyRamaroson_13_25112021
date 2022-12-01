4a0e5129aedb45f8c79eb49621be840c3ba2e0cd
Andy Ramaroson <andyramaroson@gmail.com>
2022-12-01 14:39:37 +0100

    [enhancement] add data in profil page

--- argentbank/src/components/Account/Account.jsx
+++ argentbank/src/components/Account/Account.jsx
@@ -1,19 +1,19 @@
-import React from "react";
-import "./Account.scss"
+import React from 'react'
+import './Account.scss'
 
-function Card() {
+function Account({ title, amount, description }) {
   return (
-    <section class="account">
-        <div class="account-content-wrapper">
-          <h3 class="account-title">Argent Bank Checking (x8349)</h3>
-          <p class="account-amount">$2,082.79</p>
-          <p class="account-amount-description">Available Balance</p>
-        </div>
-        <div class="account-content-wrapper cta">
-          <button class="transaction-button">View transactions</button>
-        </div>
-      </section>
+    <section className="account">
+      <div className="account-content-wrapper">
+        <h3 className="account-title">{title}</h3>
+        <p className="account-amount">{amount}</p>
+        <p className="account-amount-description">{description}</p>
+      </div>
+      <div className="account-content-wrapper cta">
+        <button className="transaction-button">View transactions</button>
+      </div>
+    </section>
   )
 }
 
-export default Card
+export default Account
--- argentbank/src/pages/Profil/Profil.jsx
+++ argentbank/src/pages/Profil/Profil.jsx
@@ -1,14 +1,20 @@
 import React from 'react'
 import Account from '../../components/Account/Account'
 import Header from '../../components/Header/Header'
+import { accountData } from '../../services/data'
 
 function Profil() {
   return (
     <main className="main sign-in bg-dark">
       <Header />
-      <Account />
-      <Account />
-      <Account />
+      {accountData.map((item) => (
+        <Account
+          key={item.id}
+          title={item.title}
+          amount={item.amount}
+          description={item.description}
+        />
+      ))}
     </main>
   )
 }
